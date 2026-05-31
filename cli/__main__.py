33966490041401f78273a2c93be0aaf81b65e8b0
fredericguiose <frederic.guiose@gmail.com>
2026-05-31 14:05:23 -0300

    feat: Enhance CLI options in `cli/__main__.py` and define database schemas in `data/schemas.py`

--- cli/__main__.py
+++ cli/__main__.py
@@ -1,5 +1,15 @@
 from utils.display import menu
 
 
+actions = [
+    "Ajouter un élève",
+    "Supprimer un élève",
+    "Ajouter un livre",
+    "Suprimer un livre",
+    "Vérifier un élève",
+    "Ajouter un emprumt",
+    "Retour d'un livre",
+    "Quitter",
+]
 
-menu("Choisissez une option : ",[""])
+menu("Choisissez une option : ",actions)
--- data/schemas.py
+++ data/schemas.py
@@ -3,6 +3,20 @@ Define your following data structures for Strict-Type Programming
 Note : The first key it's reserved for the primary key and HE MUST UNIQUE !!!
 """
 
+from data.db import make_db
+
+
+__all__ = (
+    "STUDENTS",
+    "BOOKS",
+    "LOANS",
+    "SCHEMAS"
+)
+
+STUDENTS = make_db("STUDENTS")
+BOOKS = make_db("BOOKS")
+LOANS = make_db("LOANS")
+
 SCHEMAS = {
     "STUDENTS": { "id": int, "first_name": str,"last_name": str,},
     "BOOKS":{"isbn": str, "title": str, "author": str, "year_published": int,"status": bool,},
